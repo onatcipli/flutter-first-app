4dc41e5c15035f5641f69acde98ec4faad8ed552
onatipli <onatcipli@outlook.com>
2018-06-16 21:12:38 +0300

    Add first flutter app

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          alreadySaved ? _saved.remove(pair) : _saved.add(pair);
+        });
+      },
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return new Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Onat Cipli'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.map), onPressed: _pushSaved),
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+        new MaterialPageRoute(builder: (context) {
+          final tiles = _saved.map(
+              (pair){
+                return new ListTile(
+                  title: new Text(pair.asPascalCase , style: _biggerFont),
+                );
+              }
+          );
+          final divided = ListTile.divideTiles(tiles: tiles , context: context).toList();
+          return new Scaffold(
+            appBar: new AppBar(title: new Text("Favorited Selections"),),
+            body: new ListView(children: divided,),
+          );
+        })
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: ThemeData(primaryColor: Colors.white),
+      title: 'Startup Name Generator',
+      home: new RandomWords(),
+    );
+  }
+}
